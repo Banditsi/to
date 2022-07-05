4f19aee1945acee36d997624fded5a6d9fa7c7da
Banditsi <103197256+Banditsi@users.noreply.github.com>
2022-07-05 21:48:00 +0300

    Add files via upload

---
+++ TODO LIST/assets/js/script.js
@@ -0,0 +1,69 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for (var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }          
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
+
+
+
+
+ /*   const completedButton = document.createElement('button')
+        completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton); */
+
+        const filterOption = document.querySelector('.filter-todo');
+
+        filterOption.addEventListener('click', filterTodo);
+
+        function filterTodo(e){
+            const todos = todoList.childNodes;
+            todos.forEach(function(todo){
+                switch(e.target.value){
+                    case "all":
+                        todo.style.display = 'flex';
+                        break;
+                        case "completed":
+                            if(todo.classList.contains('completed')){
+                                todo.style.display = 'flex';
+                            }else{
+                                todo.style.display = 'none';
+                            }
+                            case "uncompleted":
+                                if(!todo.classList.contains("completed")){
+                                    todo.style.display = 'flex';
+                                }
+                                else{
+                                    todo.style.display = 'none';
+                                }
+                }
+            });
+        }
